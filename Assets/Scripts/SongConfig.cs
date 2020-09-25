313e62ea4f460e9899b53a01a7ff83859994df84
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-09-25 14:47:50 +0200

    fixes and song config

---
+++ Assets/Scripts/SongConfig.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongConfig : MonoBehaviour
+{
+    [SerializeField] public Attacks[] attacks;
+
+    public bool playing;
+
+    private void Update()
+    {
+        if (playing)
+            for (int i = 0; i < attacks.Length; i++)
+            {
+                attacks[i].second -= Time.deltaTime;
+
+                if(attacks[i].second <= 0)
+                {
+                    SendNewInstrument(i);
+                }
+            }
+    }
+
+    public void SendNewInstrument(int i)
+    {
+        print(attacks[i].instrument.ToString());
+    }
+}
+
+public enum InstrumentAttack
+{
+    guitar, percussion, drum, bass, synth, piano, trumpet, voice, hooter, drop
+}
+
+[System.Serializable]
+public struct Attacks
+{
+    public InstrumentAttack instrument;
+    public float second;
+}
