eeea308045bdb978859efd4ef06a9a952148b5a7
MarcM-collab <58913562+MarcM-collab@users.noreply.github.com>
2020-09-28 17:50:08 +0200

    tuto

---
+++ Assets/Tuto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuto : MonoBehaviour
+{
+    private int i = 0;
+    public List<GameObject> g = new List<GameObject>();
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            i++;
+            for (int i1 = 0; i1 < g.Count; i1++)
+            {
+                g[i1].SetActive(false);
+            }
+            g[i].SetActive(true);
+        }
+        if (i == g.Count)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
